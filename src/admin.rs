2af6634f923db9f62a6f6e365fb312d5e4a40850
Tiago Nascimento <tiago@theosirian.com>
2018-03-16 12:23:53 -0300

    version used in the event

--- src/admin.rs
+++ src/admin.rs
@@ -1,9 +1,14 @@
-extern crate rocket;
-extern crate rocket_contrib;
+use ::std::collections::HashMap;
+use ::rocket::State;
+use ::rocket::http::{Cookie, Cookies};
+use ::rocket::response::Redirect;
+use ::rocket::request::Form;
+use ::rocket_contrib::Template;
 
-use std::collections::HashMap;
-use rocket::response::Redirect;
-use rocket_contrib::{Json, Template};
+extern crate bcrypt;
+use self::bcrypt::verify;
+
+const PASSWORD: &'static str = "$2y$12$HdiZ1FybVjyzFqOElZneVuDIHykYgTCSWxoBjMtYgx1BrrzQoAwJq";
 
 #[get("/admin")]
 fn get_login() -> Template {
@@ -11,16 +16,88 @@ fn get_login() -> Template {
    Template::render("admin_login", &ctx)
 }
 
-#[derive(Deserialize)]
+#[derive(FromForm)]
 struct LoginData {
    password: String,
 }
 
-#[post("/admin", format = "application/json", data = "<data>")]
-fn post_login(data: Json<LoginData>) -> Redirect {
-   if data.0.password == "adminpassword123" {
-      Redirect::to("/")
-   } else {
-      Redirect::to("/admin")
+#[post("/admin", data = "<data>")]
+fn post_login(data: Form<LoginData>, mut cookies: Cookies) -> Redirect {
+   match verify(&data.get().password, PASSWORD) {
+      Ok(_)  => {
+         cookies.add_private(Cookie::new("auth", "true"));
+         Redirect::to("/")
+      },
+      Err(_) => Redirect::to("/admin"),
+   }
+}
+
+#[get("/points")]
+fn get_points() -> Template {
+   let ctx: HashMap<&str, &str> = HashMap::new();
+   Template::render("admin_points", &ctx)
+}
+
+#[derive(FromForm)]
+struct PointsData {
+   team: String,
+   value: isize,
+}
+
+#[post("/points", data = "<data>")]
+fn post_points(db: State<::DbConn>, data: Form<PointsData>, mut cookies: Cookies) -> Redirect {
+   match cookies.get_private("auth") {
+      Some(cookie) => {
+         match cookie.value() {
+            "true" => {
+               let value = &data.get().value;
+               if let Err(_) = match &data.get().team as &str {
+                  "r" => {
+                     db.lock().expect("lock db").r += value;
+                     ::std::fs::write("red",
+                        db.lock().expect("lock db").r.to_string() + &"\n").expect("red");
+                     Ok(())
+                  },
+                  "g" => {
+                     db.lock().expect("lock db").g += value;
+                     ::std::fs::write("green",
+                        db.lock().expect("lock db").g.to_string() + &"\n").expect("green");
+                     Ok(())
+                  },
+                  "b" => {
+                     db.lock().expect("lock db").b += value;
+                     ::std::fs::write("blue",
+                        db.lock().expect("lock db").b.to_string() + &"\n").expect("blue");
+                     Ok(())
+                  },
+                  "c" => {
+                     db.lock().expect("lock db").c += value;
+                     ::std::fs::write("cyan",
+                        db.lock().expect("lock db").c.to_string() + &"\n").expect("cyan");
+                     Ok(())
+                  },
+                  "m" => {
+                     db.lock().expect("lock db").m += value;
+                     ::std::fs::write("magenta",
+                        db.lock().expect("lock db").m.to_string() + &"\n").expect("magenta");
+                     Ok(())
+                  },
+                  "y" => {
+                     db.lock().expect("lock db").y += value;
+                     ::std::fs::write("yellow",
+                        db.lock().expect("lock db").y.to_string() + &"\n").expect("yellow");
+                     Ok(())
+                  },
+                  _ => Err(()),
+               } {
+                  Redirect::to("/error1")
+               } else {
+                  Redirect::to("/points")
+               }
+            },
+            _ => Redirect::to("/error2"),
+         }
+      },
+      None => Redirect::to("/error3"),
    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,52 @@
-#![feature(plugin)]
+#![feature(plugin, custom_derive)]
+#![feature(fs_read_write)]
 #![plugin(rocket_codegen)]
 
 extern crate rand;
 extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
+extern crate serde;
 #[macro_use] extern crate serde_derive;
 
+use std::sync::Mutex;
 use std::path::{Path, PathBuf};
+use std::fs::File;
+use std::io::Read;
 use rand::{Rng};
+use rocket::State;
 use rocket::response::NamedFile;
 use rocket_contrib::{Json, Template};
 
 mod admin;
+mod quiz;
 
 #[derive(Serialize)]
 struct DashboardContext {
-   teams: [(String, String, u32);6],
-   factor: u32,
+   teams: [(String, String, isize);6],
+   factor: usize,
 }
 
 #[get("/")]
-fn home() -> Template {
+fn home(db: State<DbConn>) -> Template {
+   let teams = db.lock().expect("show dashboard");
    let context = DashboardContext {
-      teams: [("ÉRRE".to_string(), "red".to_string(), 1000),
-               ("GÊ".to_string(), "green".to_string(), 2000),
-               ("BÊ".to_string(), "blue".to_string(), 3000),
-               ("CÊ".to_string(), "cyan".to_string(), 4000),
-               ("ÊME".to_string(), "magenta".to_string(), 5000),
-               ("UAI".to_string(), "yellow".to_string(), 6000)],
-      factor: 20000,
+      teams: [("ÉRRE".to_string(), "red".to_string(), teams.r),
+               ("GÊ".to_string(), "green".to_string(), teams.g),
+               ("BÊ".to_string(), "blue".to_string(), teams.b),
+               ("CÊ".to_string(), "cyan".to_string(), teams.c),
+               ("ÊME".to_string(), "magenta".to_string(), teams.m),
+               ("UAI".to_string(), "yellow".to_string(), teams.y)],
+      factor: 4000,
    };
    Template::render("dashboard", &context)
 }
 
 #[get("/dashboard")]
-fn dashboard() -> Json {
-   let factor = 20000;
+fn dashboard(db: State<DbConn>) -> Json {
+   let factor = 4000;
+   let teams = db.lock().expect("show dashboard");
 	Json(json!({
-		"points": [rand::thread_rng().gen_range(1000, factor),
-                  rand::thread_rng().gen_range(1000, factor),
-                  rand::thread_rng().gen_range(1000, factor),
-                  rand::thread_rng().gen_range(1000, factor),
-                  rand::thread_rng().gen_range(1000, factor),
-                  rand::thread_rng().gen_range(1000, factor)],
+      "points": [teams.r, teams.g, teams.b, teams.c, teams.m, teams.y,],
 		"factor": factor,
 	}))
 }
@@ -52,13 +56,56 @@ fn static_files(file: PathBuf) -> Option<NamedFile> {
    NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
+struct Teams {
+   r: isize,
+   g: isize,
+   b: isize,
+   c: isize,
+   m: isize,
+   y: isize,
+}
+
+type DbConn = Mutex<Teams>;
+static NAMES: [&'static str; 6] = ["red", "green", "blue", "cyan", "magenta", "yellow"];
+
 fn main() {
+   let mut teams = [0, 0, 0, 0, 0, 0];
+
+   for i in 0..NAMES.len() {
+      let mut data = match std::fs::read_string(NAMES[i]) {
+         Ok(val) => val,
+         Err(_) => {
+            std::fs::write(NAMES[i], "0\n").expect(NAMES[i]);
+            "0".to_string()
+         },
+      };
+      data.pop();
+      teams[i] = match data.parse() {
+         Ok(n) => n,
+         Err(_) => 0,
+      }
+   }
+
+   let teams = Teams{
+      r: teams[0],
+      g: teams[1],
+      b: teams[2],
+      c: teams[3],
+      m: teams[4],
+      y: teams[5],
+   };
+   
+   let db = Mutex::new(teams);
 	rocket::ignite()
+      .manage(db)
       .mount("/", routes![static_files,
                            home,
                            dashboard,
                            admin::get_login,
-                           admin::post_login])
+                           admin::post_login,
+                           admin::get_points,
+                           admin::post_points,
+                           quiz::quiz])
       .attach(Template::fairing())
       .launch();
 }
